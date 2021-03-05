e67f07a26da8ffc689d963581ed2753522c58521
Nathan Keith <nashvegasnate@gmail.com>
2021-03-05 11:42:15 -0600

    added fight functionality

--- src/javascripts/helpers/buttonEvents.js
+++ src/javascripts/helpers/buttonEvents.js
@@ -1,13 +1,14 @@
 import { eatFunc } from '../components/eat';
 import { playFunc } from '../components/play';
+import { fightFunc } from '../components/fight';
 
 const buttonEvents = () => {
   document.querySelector('#goodFood').addEventListener('click', eatFunc);
   document.querySelector('#junkFood').addEventListener('click', eatFunc);
   document.querySelector('#bigFun').addEventListener('click', playFunc);
   document.querySelector('#smallFun').addEventListener('click', playFunc);
-  // document.querySelector('#run').addEventListener('click', fightFunc);
-  // document.querySelector('#violence').addEventListener('click', fightFunc);
+  document.querySelector('#run').addEventListener('click', fightFunc);
+  document.querySelector('#violence').addEventListener('click', fightFunc);
   // document.querySelector('#nap').addEventListener('click', sleepFunc);
   // document.querySelector('#slumber').addEventListener('click', sleepFunc);
 };
