d3366bf92b1750dba8ed765cfc55efc5a96c501f
Nathan Keith <nashvegasnate@gmail.com>
2021-03-05 21:34:42 -0600

    added styling

--- src/javascripts/components/domBuilder.js
+++ src/javascripts/components/domBuilder.js
@@ -1,8 +1,8 @@
 const domBuilder = () => {
   document.querySelector('#eat').innerHTML = `<div id='main-container'>
-                                                <div id='eatName' class='d-flex justify-content-center'>EAT</div>
+                                                <div id='eatTitle' class='d-flex justify-content-center'>EAT</div>
                                                 <hr>
-                                                <div id='eatScore'>100</div>
+                                                <div id='eatScore' class='d-flex justify-content-center'>100</div>
                                                 <hr>
                                                 <div class='d-flex justify-content-around'>
                                                 <button id='goodFood' class='btn btn-dark'>HEALTHY<br>+10</button>
@@ -10,9 +10,9 @@ const domBuilder = () => {
                                                 </div>
                                               </div>`;
   document.querySelector('#play').innerHTML = `<div id='main-container'>
-                                                <div id='playTitle'>PLAY</div>
+                                                <div id='playTitle' class='d-flex justify-content-center'>PLAY</div>
                                                 <hr>
-                                                <div id='playScore'>50</div>
+                                                <div id='playScore' class='d-flex justify-content-center'>50</div>
                                                 <hr>
                                                 <div class='d-flex justify-content-around'>
                                                 <button id='bigFun' class='btn btn-dark'>SUPER FUN!<br>+50</button>
@@ -20,9 +20,9 @@ const domBuilder = () => {
                                                 </div>
                                               </div>`;
   document.querySelector('#fight').innerHTML = `<div id='main-container'>
-                                                  <div id='fightTitle'>FIGHT</div>
+                                                  <div id='fightTitle' class='d-flex justify-content-center'>FIGHT</div>
                                                   <hr>
-                                                  <div id='fightScore'>100</div>
+                                                  <div id='fightScore' class='d-flex justify-content-center'>100</div>
                                                   <hr>
                                                   <div class='d-flex justify-content-around'>
                                                   <button id='run' class='btn btn-dark'>RUN AWAY<br>+1</button>
@@ -30,9 +30,9 @@ const domBuilder = () => {
                                                   </div>
                                                 </div>`;
   document.querySelector('#sleep').innerHTML = `<div id='main-container'>
-                                                  <div id='sleepTitle'>SLEEP</div>
+                                                  <div id='sleepTitle' class='d-flex justify-content-center'>SLEEP</div>
                                                   <hr>
-                                                  <div id='sleepScore'>50</div>
+                                                  <div id='sleepScore' class='text-center'>50</div>
                                                   <hr>
                                                   <div class='d-flex justify-content-around'>
                                                   <button id='nap' class='btn btn-dark'>NAP<br>+50</button>
